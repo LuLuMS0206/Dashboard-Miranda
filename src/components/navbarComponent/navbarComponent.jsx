05432dbbbc67303d22e910f84085a72679d7473c
LuLuMS0206 <luciamacho00@gmail.com>
2024-06-25 18:53:26 +0200

    add login

--- src/components/navbarComponent/navbarComponent.jsx
+++ src/components/navbarComponent/navbarComponent.jsx
@@ -11,7 +11,7 @@ import { GrContactInfo } from "react-icons/gr";
 import { FooterStyles, UserStyles, PLiStyles, MenuLeftStyles, NavbarStyles, PStyles, DivLeftStyles, LogoContentStyles, ImgLogoStyles, TravelStyles, HotelStyles, LiStyles, UlStyles } from "./navbarStyles";
 import { useState } from 'react';
 import './stylesNavbar.css';
-import { CardComponent } from "../cardComponent/cardComponent";
+
 
 
 export const NavbarComponent = () => {
@@ -24,88 +24,95 @@ export const NavbarComponent = () => {
     };
 
     return (
-<> 
-        <NavbarStyles>
-            <DivLeftStyles>
-                {menuOpen && (
-                    <MenuLeftStyles>
-                        <div >
-                            <LogoContentStyles>
-                                <ImgLogoStyles src="src/assets/img/logo.png" alt="" />
-                                <div>
-                                    <TravelStyles>travl</TravelStyles>
-                                    <HotelStyles>Hotel Admin Dashboard</HotelStyles>
-                                </div>
+        <>
+            <NavbarStyles>
+                <DivLeftStyles>
+                    {menuOpen && (
+                        <MenuLeftStyles>
+                            <div >
+                                <LogoContentStyles>
+                                    <ImgLogoStyles src="src/assets/img/logo.png" alt="" />
+                                    <div>
+                                        <TravelStyles>travl</TravelStyles>
+                                        <HotelStyles>Hotel Admin Dashboard</HotelStyles>
+                                    </div>
 
-                            </LogoContentStyles>
+                                </LogoContentStyles>
 
-                            <UlStyles>
-                                <LiStyles>
-                                    <RiDashboardLine />
-                                    <PLiStyles>Dashboard</PLiStyles>
-                                </LiStyles>
+                                <UlStyles>
+                                    <LiStyles>
+                                        <RiDashboardLine />
+                                        <PLiStyles>Dashboard</PLiStyles>
+                                    </LiStyles>
 
-                                <LiStyles>
-                                    <TbCalendarCheck />
-                                    <PLiStyles>Bookings</PLiStyles>
-                                </LiStyles>
+                                    <LiStyles>
+                                        <TbCalendarCheck />
+                                        <PLiStyles>Bookings</PLiStyles>
+                                    </LiStyles>
 
-                                <LiStyles>
-                                    <SlKey />
-                                    <PLiStyles>Room</PLiStyles>
-                                </LiStyles>
+                                    <LiStyles>
+                                        <SlKey />
+                                        <PLiStyles>Room</PLiStyles>
+                                    </LiStyles>
 
-                                <LiStyles>
-                                    <GrContactInfo />
-                                    <PLiStyles>Contact</PLiStyles>
-                                </LiStyles>
+                                    <LiStyles>
+                                        <GrContactInfo />
+                                        <PLiStyles>Contact</PLiStyles>
+                                    </LiStyles>
 
-                                <LiStyles>
-                                    <BsPerson />
-                                    <PLiStyles>Users</PLiStyles>
-                                </LiStyles>
-                            </UlStyles>
+                                    <LiStyles>
+                                        <BsPerson />
+                                        <PLiStyles>Users</PLiStyles>
+                                    </LiStyles>
+                                </UlStyles>
 
-                            <UserStyles>
-                                <img src="/assets/user.jpeg" />
-                                <p>Lucia Macho Sánchez</p>
-                                <p>luciamacho00@gmail.com</p>
-                                <button>Contact Us</button>
-                            </UserStyles>
-                            <FooterStyles>
-                                <p>Travl Hotel Admin Dashboard</p>
-                                <p>© 2020 All Rights Reserved</p>
-                                <p>Made by Lucia Macho</p>
-                            </FooterStyles>
+                                <UserStyles>
+                                    <img src="/assets/user.jpeg" />
+                                    <p>Lucia Macho Sánchez</p>
+                                    <p>luciamacho00@gmail.com</p>
+                                    <button>Contact Us</button>
+                                </UserStyles>
+                                <FooterStyles>
+                                    <p>Travl Hotel Admin Dashboard</p>
+                                    <p>© 2020 All Rights Reserved</p>
+                                    <p>Made by Lucia Macho</p>
+                                </FooterStyles>
 
-                        </div>
-                    </MenuLeftStyles>
-                )}
-
-
-
-                <TfiAlignLeft onClick={handleClick} />
-                <PStyles>Dashboard</PStyles>
-            </DivLeftStyles>
+                            </div>
+                        </MenuLeftStyles>
+                    )}
 
 
 
-            <div>
-                <IoIosSearch className="icons" />
 
-                <IoMdHeartEmpty className="icons" />
+                </DivLeftStyles>
+
+                <div>
+                    <div>
+                        <TfiAlignLeft onClick={handleClick} />
+                        <PStyles>Dashboard</PStyles>
+                    </div>
 
 
-                <MdOutlineMail className="icons" />
 
-                <TbMessage className="icons" />
+                    <div>
+                        <IoIosSearch className="icons" />
 
-                <img src="" alt="Profile icon" />
+                        <IoMdHeartEmpty className="icons" />
 
-            </div>
-        </NavbarStyles>
-        <CardComponent/>
-   </>
+
+                        <MdOutlineMail className="icons" />
+
+                        <TbMessage className="icons" />
+
+                        <img src="" alt="Profile icon" />
+
+                    </div>
+                </div>
+
+            </NavbarStyles>
+
+        </>
 
     )
 }
