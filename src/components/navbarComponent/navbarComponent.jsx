615d16e92e9b291dbd89cc705ab923a84283f636
LuLuMS0206 <luciamacho00@gmail.com>
2024-06-25 12:27:37 +0200

    navbar hambur and styles

--- src/components/navbarComponent/navbarComponent.jsx
+++ src/components/navbarComponent/navbarComponent.jsx
@@ -3,31 +3,58 @@ import { IoIosSearch } from "react-icons/io";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { MdOutlineMail } from "react-icons/md";
 import { TbMessage } from "react-icons/tb";
-import { DivLeftStyles, NavbarStyles, PStyles } from "../navbarStyles";
-
+import { MenuLeftStyles, NavbarStyles, PStyles, DivLeftStyles, LogoContentStyles, ImgLogoStyles, TravelStyles, HotelStyles } from "./navbarStyles";
+import { useState } from 'react';
+import './stylesNavbar.css';
 
 
 export const NavbarComponent = () => {
 
+    const [menuOpen, setIsMenulOpen] = useState(false);
+
+
+    const handleClick = () => {
+        setIsMenulOpen(!menuOpen);
+    };
+
     return (
 
         <NavbarStyles>
-
             <DivLeftStyles>
-                <TfiAlignLeft />
+                {menuOpen && (
+                    <MenuLeftStyles>
+                        <div className="NavbarOpen">
+                            <LogoContentStyles>
+                            <ImgLogoStyles src="src/assets/img/logo.png" alt="" />
+                            <div>
+                            <TravelStyles>travl</TravelStyles>
+                            <HotelStyles>Hotel Admin Dashboard</HotelStyles>
+                            </div>
+                                
+                            </LogoContentStyles>
+
+                        </div>
+                    </MenuLeftStyles>
+                )}
+
+
+
+                <TfiAlignLeft onClick={handleClick} />
                 <PStyles>Dashboard</PStyles>
             </DivLeftStyles>
 
 
+
             <div>
-                <IoIosSearch />
+                <IoIosSearch className="icons" />
 
-                <IoMdHeartEmpty />
+                <IoMdHeartEmpty className="icons" />
 
 
-                <MdOutlineMail />
+                <MdOutlineMail className="icons" />
 
-                <TbMessage />
+                <TbMessage className="icons" />
+
                 <img src="" alt="Profile icon" />
 
             </div>
