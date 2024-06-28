98a277b8373f8274d8372e8b6c1c458307e91081
LuLuMS0206 <luciamacho00@gmail.com>
2024-06-28 09:25:08 +0200

    add pages

---
+++ src/pages/contactPage/contactPage.jsx
@@ -0,0 +1,10 @@
+import { NavbarComponent } from "../../components/navbarComponent/navbarComponent"
+
+
+export const ContactPage = () => {
+    return(
+        <NavbarComponent>
+            
+        </NavbarComponent>
+    )
+}
---
+++ src/pages/roomPage/roomPage.jsx
@@ -0,0 +1,12 @@
+
+import { NavbarComponent } from "../../components/navbarComponent/navbarComponent"
+
+
+
+export const RoomPage = () => {
+    return(
+        <NavbarComponent>
+          
+        </NavbarComponent>
+    )
+}
