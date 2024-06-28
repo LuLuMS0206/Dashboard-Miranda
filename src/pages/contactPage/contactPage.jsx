88d02d3e4ccbba371c54745ed40a4bdf981492e2
LuLuMS0206 <luciamacho00@gmail.com>
2024-06-28 18:55:44 +0200

    end tables pages

--- src/pages/contactPage/contactPage.jsx
+++ src/pages/contactPage/contactPage.jsx
@@ -1,10 +1,30 @@
 import { NavbarComponent } from "../../components/navbarComponent/navbarComponent"
+import { TableComponent } from "../../components/tableComponent/tableComponent"
+import contactData from "./../../data/contact.json"
 
 
 export const ContactPage = () => {
+
+    const contactColumns = [
+        { headerColumn: 'Order ID', columnsData: 'id' },
+        { headerColumn: 'Date', columnsData: 'date' },
+        { headerColumn: 'Customer',
+            
+            renderColumn: (rowData) => (
+                <div>
+                    <div>{rowData.client.name}</div>
+                    <div>{rowData.client.email}</div>
+                </div>
+            ), 
+        },
+        { headerColumn: 'Subject', columnsData: 'subject' },
+        { headerColumn: 'Comment', columnsData: 'comment' },
+        { headerColumn: 'Action', columnsData: 'action'}
+    ];
+
     return(
         <NavbarComponent>
-            
+            <TableComponent columns={contactColumns} data={contactData} />
         </NavbarComponent>
     )
 }
--- src/pages/userPage/userPage.jsx
+++ src/pages/userPage/userPage.jsx
@@ -1,10 +1,25 @@
 import { NavbarComponent } from "../../components/navbarComponent/navbarComponent"
+import { TableComponent } from "../../components/tableComponent/tableComponent"
+import userData from "./../../data/users.json"
 
 
 export const UserPage = () => {
+
+    const userColumns = [
+        { headerColumn: 'Foto', renderColumn: (rowData) => <img src={rowData.foto} alt="User" style={{ width: '50px', height: 'auto' }} /> },
+        { headerColumn: 'Nombre completo', columnsData: 'name' },
+        { headerColumn: 'ID de empleado', columnsData: 'id' },
+        { headerColumn: 'Email', columnsData: 'email' },
+        { headerColumn: 'Start Date', columnsData: 'startDate' },
+        { headerColumn: 'Description', columnsData: 'description' },
+        { headerColumn: 'Contact', columnsData: 'contact' },
+        { headerColumn: 'Status', columnsData: 'status' }
+    ];
+
+
     return(
         <NavbarComponent>
-            
+            <TableComponent columns={userColumns} data={userData} />
         </NavbarComponent>
     )
 }
