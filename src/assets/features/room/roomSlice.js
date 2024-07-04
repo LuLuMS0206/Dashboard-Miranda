40c80387ec8964cfa0bbd82ea2e92add953dc873
LuLuMS0206 <luciamacho00@gmail.com>
2024-07-04 22:22:08 +0200

    edit filters

--- src/assets/features/room/roomSlice.js
+++ src/assets/features/room/roomSlice.js
@@ -1,7 +1,5 @@
-
 import { createSlice } from '@reduxjs/toolkit';
 import { RoomsThunk } from './roomThunk';
-
 
 export const RoomSlice = createSlice({
     name: 'rooms',
@@ -12,6 +10,18 @@ export const RoomSlice = createSlice({
         room: null,
     },
     reducers: {
+        addRoom: (state, action) => {
+            state.rooms.push(action.payload);
+        },
+        editRoom: (state, action) => {
+            const index = state.rooms.findIndex(room => room.id === action.payload.id);
+            if (index !== -1) {
+                state.rooms[index] = action.payload;
+            }
+        },
+        deleteRoom: (state, action) => {
+            state.rooms = state.rooms.filter(room => room.id !== action.payload);
+        },
     },
     extraReducers: (builder) => {
         builder
@@ -20,7 +30,7 @@ export const RoomSlice = createSlice({
             })
             .addCase(RoomsThunk.fulfilled, (state, action) => {
                 state.status = 'fulfilled';
-                state.bookings = action.payload;
+                state.rooms = action.payload; 
             })
             .addCase(RoomsThunk.rejected, (state, action) => {
                 state.status = 'rejected';
@@ -29,8 +39,10 @@ export const RoomSlice = createSlice({
     },
 });
 
+export const { addRoom, editRoom, deleteRoom } = RoomSlice.actions;
 
 export const getRoomSlice = (state) => state.rooms.rooms;
 export const getRoom = (state) => state.rooms.room;
 export const getRoomsStatus = (state) => state.rooms.status;
 export const getRoomsError = (state) => state.rooms.error;
+
