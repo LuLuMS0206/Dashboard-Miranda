fb45dd4c94fb748f8f2c22f6c7b4442fde83d7b5
LuLuMS0206 <luciamacho00@gmail.com>
2024-07-09 17:17:02 +0200

    add test button

---
+++ src/components/buttonComponent/buttonComponent.test.js
@@ -0,0 +1,23 @@
+import { ButtonStyles } from './buttonComponent';
+import "jest-environment-jsdom";
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+describe('ButtonStyles component', () => {
+  test('renders with prev style', () => {
+    render(<ButtonStyles styled="prev">Previous</ButtonStyles>);
+    const button = screen.getByText('Previous');
+    expect(button).toHaveStyle('background-color: #FFFFFF');
+    expect(button).toHaveStyle('color: #135846');
+    expect(button).toHaveStyle('border: 1px solid #135846');
+  });
+
+  test('renders with next style', () => {
+    render(<ButtonStyles styled="next">Next</ButtonStyles>);
+    const button = screen.getByText('Next');
+    expect(button).toHaveStyle('background-color: #135846');
+    expect(button).toHaveStyle('color: #FFFFFF');
+    expect(button).toHaveStyle('border: none');
+  });
+});
